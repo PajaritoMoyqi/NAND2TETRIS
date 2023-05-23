69f4fb2c1a4519f23a9433d60616827d956092ec
PajaritoMoyqi <PajaritoMoyqi@protonmail.ch>
2023-05-23 14:22:46 +0900

    chapter 4 example code

--- ch04-Hack-Machine-Language/examples/Sum1ToN.asm
+++ ch04-Hack-Machine-Language/examples/Sum1ToN.asm
@@ -32,8 +32,25 @@
     D=D-M // ?
     @STOP
     D;JGT // Jump if Greater Than
+    // sum = sum + i
     @sum
     D=M // put RAM[sum] value into D register
     @i
-    D=D+M
-    
+    D=D+M // D register = D register(RAM[sum]) + RAM[i]
+    @sum
+    M=D // put D register value (after sum) into RAM[sum]
+    // i = i + 1
+    @i
+    M=M+1 // RAM[i] = RAM[i] + 1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(STOP)
+    // R1 = sum
+    @sum
+    D=M // D register = RAM[sum]
+    @R1 // R1 (virtual) register is selected
+    M=D // RAM[R1] = D register(RAM[sum])
+(END) // end of the code
+    @END
+    0;JMP
